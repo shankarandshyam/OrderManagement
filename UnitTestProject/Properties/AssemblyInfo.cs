61d8402a2a655f38c33999514f9b0149a3883883
Shyam Yaddanapudi <Shyam_Yaddanapudi@epam.com>
2019-11-26 17:07:28 +0530

    added unit test project

---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("67a163db-1aab-4ba4-b026-6bceac337991")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Order_Management.Controllers;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var controller = new OrderController();
+            var response = controller.DisplayAllOrders();
+            Assert.IsTrue(response.Count > 1);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var controller = new OrderController();
+            var response = controller.DisplayAllOrders();
+            Assert.IsNotNull(response);
+        }
+    }
+}
